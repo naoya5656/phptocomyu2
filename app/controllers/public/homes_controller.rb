18ecf30acc86ee1e98dfd8c55250392b0c6a29bc
EC2 Default User <ec2-user@ip-172-31-30-235.ap-northeast-1.compute.internal>
2023-02-24 05:02:56 +0000

    2/24

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @photos = Photo.all.order("id DESC")
+    @photo = Photo.order(created_at: :desc).limit(4)
   end
 
   def about
